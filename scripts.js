f02c6e9305c1b542e812f13fbf05a773b09f69c3
sarahRosannaBusch <annarose0113@gmail.com>
2016-07-12 16:30:26 -0700

    template complete
    header, footer, and nav load with scripts on every page

--- scripts.js
+++ scripts.js
@@ -1,34 +1,19 @@
-function onload()
-{
-  document.getElementById("header").innerHTML =
-    "<h1> Budgie Blog </h1>" +
-    "<p class='subtitle'>" +
-      "Life with Apollo the Budgerigar" +
-    "</p>"
-    ;
+document.getElementById("header").innerHTML =
+  "<h1> Budgie Blog </h1>" +
+  "<p class='subtitle'>" +
+    "Life with Apollo the Budgerigar" +
+  "</p>"
+  ;
 
-  document.getElementById("contentList").innerHTML =
-    "<h2 id='listTitle'> Table of Contents </h2>" +
-    "<a href='index.html'><div class='topic'>" +
-      "<h3 class=topic'> Home </h3>" +
-    "</div></a>" +
-    "<div class='topic'>" +
-      "<h3 class='topic'> Why I got a budgie </h3>" +
-    "</div>" +
-    "<ul>" +
-      "<li><a href='1.1_AlexTheParrot.html'> Alex the Parrot </a></li>" +
-    "</ul>" +
-    "<div class='topic'>" +
-      "<h3 class='topic'> Choosing Apollo </h3>" +
-    "</div>" +
-    "<div class='topic'>" +
-      "<h3 class='topic'> Cage, toys, and perches </h3>" +
-    "</div>" +
-    "<div class='topic'>" +
-      "<h3 class='topic'> Food and poop </h3>" +
-    "</div>" +
-    "<div class='topic'>" +
-      "<h3 class='topic'> Taming and training </h3>"
-    "</div>"
-    ;
-}
+document.getElementById("contentList").innerHTML =
+  "<h2 id='listTitle'> Table of Contents </h2>" +
+  "<a href='index.html' class='topic'><div class='topic'> Home </div></a>" +
+  "<a href='1-0.html' class='topic'><div class='topic'> Why I got a budgie </div></a>" +
+  "<a href='2-0.html' class='topic'><div class='topic'> Cage, toys, and perches </div></a>" +
+  "<a href='comingSoon.html' class='topic'><div class='topic'> Food and poop </div></a>" +
+  "<a href='comingSoon.html' class='topic'><div class='topic'> Taming and training </div></a>"
+  ;
+
+document.getElementById("footer").innerHTML =
+  "<hr>" 
+  ;
